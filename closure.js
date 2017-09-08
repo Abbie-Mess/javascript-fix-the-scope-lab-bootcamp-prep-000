4ad8d138bc2259cdca30b6f012968aa9c2810009
Abigail Messmer <abbie.messmer@gmail.com>
2017-09-08 14:59:12 +0000

    automatically backed up by learn

---
+++ closure.js
@@ -0,0 +1,10 @@
+function celebrityName(firstName){
+  var nameIntro = "This celebrity is ";
+  function lastName(theLastName){
+    return name Intro + firstName + " " + theLastName;
+  }
+  return lastName
+}
+
+var mjName - celebrityName("Michael")
+mjName = ("Jackson")
